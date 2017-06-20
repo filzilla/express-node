a126fd6df6646660b7942e44014d57f13d4d085b
filzilla <phillipdelia@Phillips-MacBook-Pro.local>
2017-06-20 16:39:22 -0400

    finished

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let application = express();
 
 
 application.get('/lorem',function(request, response){
-    response.use( loremIpsum({
+    response.send( loremIpsum({
     count: 3                      // Number of words, sentences, or paragraphs to generate. 
   , units: 'paragraphs'            // Generate words, sentences, or paragraphs. 
   , sentenceLowerBound: 5         // Minimum words per sentence. 
@@ -15,8 +15,7 @@ application.get('/lorem',function(request, response){
   , paragraphLowerBound: 5        // Minimum sentences per paragraph. 
   , paragraphUpperBound: 7        // Maximum sentences per paragraph. 
   , format: 'html'               // Plain text or html 
-  , random: Math.random           // A PRNG function. Uses Math.random by default 
-  , suffix: EOL                   // The character to insert between paragraphs. Defaults to default EOL for your OS. 
+ 
 }))
     
 });
